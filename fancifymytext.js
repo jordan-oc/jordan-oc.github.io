7f263e4f8a79a5b03919f6e015235c3c50dbe7cc
jordan-oc <jordanocon@yahoo.com>
2022-10-13 15:11:48 -0700

    4

---
+++ fancifymytext.js
@@ -0,0 +1,39 @@
+function bigger(){
+    document.getElementById("freeform").style.fontSize = "25px";
+  }
+  
+  function moo(){
+    const message = document.getElementById("freeform");
+    var str = message.value;
+    var parts = str.split(/ /);
+    for(let i = 0; i < parts.length ; i++){
+      parts[i] += "Moo";
+    }
+    str = parts.join("_");
+    message.value = str;
+    document.getElementById("freeform").style.textTransform = "uppercase";
+  
+  }
+  
+  function fancy(){
+    document.getElementById("freeform").style.fontWeight = "bold";
+    document.getElementById("freeform").style.color = "yellow";
+    document.getElementById("freeform").style.textDecoration = "italic"
+  }
+  
+  function boring(){
+    document.getElementById("freeform").style.fontWeight = "normal";
+    document.getElementById("freeform").style.color = "black";
+    document.getElementById("freeform").style.textDecoration = "none"
+  }
+  
+  function checkValue(value){
+    var fancy;
+    var boring;
+  
+    if(value === "fancy"){
+      return fancy;
+    } else if (value === "boring") {
+      return boring;
+    }
+  }
