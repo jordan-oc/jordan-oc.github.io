964012451e93f4b62b41d73f6d391f0983742b37
jordan-oc <jordanocon@yahoo.com>
2022-11-03 08:06:04 -0700

    .

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -1,34 +1,25 @@
+function bigger(){
+    let text = document.getElementById("feedme");
+    text.style.fontSize = "24pt";
+}
 
-function bigger() {
-	var textarea = document.getElementById("textarea");
-	textarea.style.fontSize = "24pt";
-	hello();
-  }
-  
-  function fancy() {
-	var clicked = document.getElementById("fancy");
-	var textarea = document.getElementById("textarea");
-	if (clicked.checked) {
-	  hello();
-	  textarea.style.fontWeight = "bold";
-	  textarea.style.color = "blue";
-	  textarea.style.textDecoration = "underline";
-	} else {
-	  hello();
-	  textarea.style.fontWeight = "normal";
-	  textarea.style.color = "black";
-	  textarea.style.textDecoration = "none";
-	}
-  }
-  
-  function hello() {
-	alert("Hello, World!");
-  }
-  
-  function moo() {
-	var textarea = document.getElementById("textarea");
-	textarea.style.textTransform = "uppercase";
-	var period = textarea.value.split(".");
-	textarea.value = period.join("-Moo");
-	hello();
-  }
+function bold(){
+    let text = document.getElementById("feedme");
+    text.style.fontWeight = "bold";
+    text.style.color = "blue";
+    text.style.textDecoration = "underline";
+}
+
+function boring(){
+    let text = document.getElementById("feedme");
+    text.style.fontSize = "initial";
+    text.style.fontWeight = "normal"
+    text.style.color = "black";
+    text.style.textDecoration = "none";
+}
+
+function moo(){
+    let text = document.getElementById("feedme");
+    text.style.textTransform = "uppercase";
+    text.value = text.value.split('.').join('') + '-moo';
+}
